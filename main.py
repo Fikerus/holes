14fa35fbfcb79e82fbf12b90d1608a4410516e7b
Fikerus <https://github.com/Fikerus>
2022-10-08 04:05:18 +0300

    Fixed for unsupported symbols

--- main.py
+++ main.py
@@ -3,8 +3,24 @@ from table import table
 
 def main():
     args = ' '.join(argv[1:])
-    print(args)
-    print(sum(map(lambda x: table[x], args)))
+    print(f'Input: {args}')
+
+    unsupported = []
+    number_of_holes = 0
+
+    for symbol in args:
+        if symbol in table:
+            number_of_holes += table[symbol]
+        else:
+            unsupported.append(symbol)
+    if not unsupported:
+        print(number_of_holes)
+    else:
+        print('There are symbols that are not supported')
+        print('Estimated number of holes:')
+        print(f'>= {number_of_holes}')
+        print('Symbols that were not counted:')
+        print(', '.join(map(lambda x: f'"{x}"', unsupported)))
 
 if __name__ == '__main__':
     main()
